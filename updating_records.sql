81a089a8a11e3ea13ed4108d7e207e4bdbd3fde7
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2025-03-30 22:59:39 -0300

    Update updating_records.sql

--- updating_records.sql
+++ updating_records.sql
@@ -1,35 +1,24 @@
 -- searching target row
-SELECT 
-	date_transaction,
-	id, 
-	product_key,
-	quantity,
-	update_on
-FROM 
-	fact_production
-WHERE 
-	product_key = 1166 AND
-	date_transaction = CONVERT(datetime, '2025-02-12 19:42:55', 120); 
+SELECT date_transaction, id, product_key, quantity, update_on
+FROM  fact_production
+WHERE product_key = 1166 AND date_transaction = '2025-02-12T19:42:55'; 
 
 -- Restoring initial record
 DELETE FROM fact_production
-WHERE product_key = 1166 AND
-	date_transaction = CONVERT(datetime, '2025-02-12 19:42:55', 120); 
+WHERE product_key = 1166 AND date_transaction = '2025-02-12T19:42:55'; 
 INSERT INTO fact_production (date_transaction, id, product_key, quantity, update_on)
-VALUES
-	(CONVERT(datetime, '2025-02-12 19:42:55', 120), 19429218, 1166, 204, CONVERT(datetime, '2025-02-12 19:43:41', 120));
+VALUES ('2025-02-12T19:42:55', 19429218, 1166, 204, '2025-02-12T19:43:41');
 
 -- Causing error
 UPDATE fact_production
 SET product_key = 1166,    
     quantity = 204000000,        
     update_on = GETDATE()  
-WHERE product_key = 1166 AND
-	date_transaction = CONVERT(datetime, '2025-02-12 19:42:55', 120); 
+WHERE product_key = 1166 AND date_transaction = '2025-02-12 19:42:55'; 
 
 -- Method Offset Entry
 INSERT INTO fact_production (date_transaction, id, product_key, quantity, update_on)
 VALUES 
-	(CONVERT(datetime, '2025-02-12 19:42:55', 120), 19429218, 1166, -204000000, GETDATE()),
-	(CONVERT(datetime, '2025-02-12 19:42:55', 120), 19429218, 1166, 204, GETDATE());
+	('2025-02-12 19:42:55', 120), 19429218, 1166, -204000000, GETDATE()),
+	('2025-02-12 19:42:55', 120), 19429218, 1166, 204, GETDATE());
 
