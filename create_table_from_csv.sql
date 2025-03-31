545b639e573e045de55e043629c1c7cb9df55fe0
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2025-03-30 23:26:13 -0300

    Create create_table_from_csv.sql

---
+++ create_table_from_csv.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE sandbox;
+GO
+
+USE sandbox;
+GO 
+
+CREATE TABLE fact_production (
+    date_transaction DATETIME,
+    id INT,
+    product_key INT,
+    quantity INT,
+    update_on DATETIME
+);
+GO
+
+BULK INSERT fact_production
+FROM 'C:/Temp/fact_production.csv'
+WITH (
+	CODEPAGE = '65001',
+	FIELDTERMINATOR = ',',
+	FIRSTROW = 2
+);
+GO
+
+SELECT TOP(100)
+    date_transaction,
+    id,
+    product_key,
+    quantity,
+    update_on
+FROM 
+	fact_production;
+);
