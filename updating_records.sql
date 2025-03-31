da3377eedd31ed888bfb62938b6f62bc65800f19
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2025-03-30 23:05:20 -0300

    Update updating_records.sql

--- updating_records.sql
+++ updating_records.sql
@@ -12,13 +12,18 @@ VALUES ('2025-02-12T19:42:55', 19429218, 1166, 204, '2025-02-12T19:43:41');
 -- Causing error
 UPDATE fact_production
 SET product_key = 1166,    
-    quantity = 204000000,        
-    update_on = GETDATE()  
+    quantity = 204000000        
+WHERE product_key = 1166 AND date_transaction = '2025-02-12T19:42:55'; 
+
+-- Correcting error
+UPDATE fact_production
+SET product_key = 1166,    
+    quantity = 204,
+	update_on = GETDATE()
 WHERE product_key = 1166 AND date_transaction = '2025-02-12T19:42:55'; 
 
 -- Method Offset Entry
 INSERT INTO fact_production (date_transaction, id, product_key, quantity, update_on)
 VALUES 
-	('2025-02-12T19:42:55', 120), 19429218, 1166, -204000000, GETDATE()),
-	('2025-02-12T19:42:55', 120), 19429218, 1166, 204, GETDATE());
-
+	('2025-02-12T19:42:55', 19429218, 1166, -204000000, GETDATE()),
+	('2025-02-12T19:42:55', 19429218, 1166, 204, GETDATE());
