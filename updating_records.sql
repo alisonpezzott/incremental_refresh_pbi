8cfd59b1b6e93eefaf991cc8c1c205a0c6f32d31
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2025-03-30 23:37:25 -0300

    Update updating_records.sql

--- updating_records.sql
+++ updating_records.sql
@@ -7,20 +7,21 @@ WHERE product_key = 1166 AND date_transaction = '2025-02-12T19:42:55';
 DELETE FROM fact_production
 WHERE product_key = 1166 AND date_transaction = '2025-02-12T19:42:55'; 
 INSERT INTO fact_production (date_transaction, id, product_key, quantity, update_on)
-VALUES ('2025-02-12T19:42:55', 19429218, 1166, 204, '2025-02-12T19:43:41');
+VALUES ('2025-02-12T19:42:55', 19429218, 1166, 204000000, '2025-02-12T19:43:41');
 
--- Causing error
-UPDATE fact_production
-SET product_key = 1166,    
-    quantity = 204000000        
-WHERE product_key = 1166 AND date_transaction = '2025-02-12T19:42:55'; 
-
--- Correcting error
+-- Correcting error without correcting date_transaction
 UPDATE fact_production
 SET product_key = 1166,    
     quantity = 204,
 	update_on = GETDATE()
 WHERE product_key = 1166 AND date_transaction = '2025-02-12T19:42:55'; 
+
+-- Correcting error correcting date_transaction
+UPDATE fact_production
+SET product_key = 1166,    
+    quantity = 204,
+	update_on = GETDATE()
+WHERE product_key = 1166 AND date_transaction = GETDATE(); 
 
 -- Method Offset Entry
 INSERT INTO fact_production (date_transaction, id, product_key, quantity, update_on)
