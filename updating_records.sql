a9193f79a3c7579ff9f7fff3a03bf0c016271c1b
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2025-03-31 01:35:03 -0300

    Update updating_records.sql

--- updating_records.sql
+++ updating_records.sql
@@ -1,27 +1,24 @@
 -- searching target row
 SELECT date_transaction, id, product_key, quantity, update_on
 FROM  fact_production
-WHERE product_key = 1166 AND date_transaction = '2025-02-12T19:42:55'; 
+WHERE id = 19429218; 
 
 -- Restoring initial record
 DELETE FROM fact_production
-WHERE product_key = 1166 AND date_transaction = '2025-02-12T19:42:55'; 
+WHERE id = 19429218; 
 INSERT INTO fact_production (date_transaction, id, product_key, quantity, update_on)
 VALUES ('2025-02-12T19:42:55', 19429218, 1166, 204000000, '2025-02-12T19:43:41');
 
 -- Correcting error without correcting date_transaction
 UPDATE fact_production
-SET product_key = 1166,    
-    quantity = 204,
-	update_on = GETDATE()
-WHERE product_key = 1166 AND date_transaction = '2025-02-12T19:42:55'; 
+SET quantity = 204
+WHERE id = 19429218; 
 
 -- Correcting error correcting date_transaction
 UPDATE fact_production
-SET product_key = 1166,    
-    quantity = 204,
+SET quantity = 204,
 	update_on = GETDATE()
-WHERE product_key = 1166 AND date_transaction = GETDATE(); 
+WHERE id = 19429218;  
 
 -- Method Offset Entry
 INSERT INTO fact_production (date_transaction, id, product_key, quantity, update_on)
